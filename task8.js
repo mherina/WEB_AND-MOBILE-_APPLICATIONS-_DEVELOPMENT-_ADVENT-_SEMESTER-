18f63bfe2840cfba777360eb6e03f978597b1ad5
Najjuuko merinah <merinagoshens@gmail.com>
2024-11-13 11:09:05 +0300

    Add files via upload

---
+++ task8.js
@@ -0,0 +1,11 @@
+//object school
+let school = {
+    name: "St_peters_nsambya",
+    schoolLocation: "nsambya",
+    establishedYear:1999,
+    studentsCount:100
+};
+
+let {name, schoolLocation,studentsCount } =school ;
+
+console.log(`${name} is located in ${schoolLocation} and has${studentsCount}students.`);
