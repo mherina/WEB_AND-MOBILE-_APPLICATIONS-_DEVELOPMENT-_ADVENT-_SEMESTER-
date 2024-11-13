2e34552c8f1feee6e171ab81e7057d341db2612f
Najjuuko merinah <merinagoshens@gmail.com>
2024-11-13 11:07:30 +0300

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,15 @@
+//object school
+let school = {
+    name: "St_peters_nsambya",
+    schoolLocation: "nsambya",
+    establishedYear:1999,
+    studentsCount:100
+};
+
+//adding an object
+school.schoolType = "Secondary";
+//modifying the studentCount
+school.studentsCount = "15";
+console.log(school);
+
+
